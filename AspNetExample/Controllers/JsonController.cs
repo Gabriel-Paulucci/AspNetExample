1da89e77994145627c80730aafe1cf5bd9a28b44
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-02-11 17:55:12 -0300

    Sistema de json

---
+++ AspNetExample/Controllers/JsonController.cs
@@ -0,0 +1,34 @@
+﻿using AspNetExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetExample.Controllers
+{
+    public class JsonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Receive(Book book)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Send()
+        {
+            return Ok(new Book
+            {
+                Id = 0,
+                Name = "Teste",
+                Description = "Um belo teste"
+            });
+        }
+    }
+}
---
+++ AspNetExample/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetExample.Models
+{
+    public class Book
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
